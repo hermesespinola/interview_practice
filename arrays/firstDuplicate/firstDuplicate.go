d0cea504a5bda3d1df874578be784c2dd05ebefd
Hermes <hermes.espinola@wizeline.com>
2018-05-16 20:58:21 -0500

    first duplicate

---
+++ arrays/firstDuplicate/firstDuplicate.go
@@ -0,0 +1,13 @@
+func firstDuplicate(a []int) int {
+    for _, v := range a {
+        if (v < 0) {
+            v = -v
+        }
+        if a[v - 1] > 0 {
+            a[v - 1] = -a[v - 1]
+        } else {
+            return v
+        }
+    }
+    return -1
+}
---
+++ arrays/firstDuplicate/firstDuplicate.py
@@ -0,0 +1,7 @@
+def firstDuplicate(a):
+    for i in xrange(len(a)):
+        if a[abs(a[i]) - 1] > 0:
+            a[abs(a[i]) - 1] *= -1
+        else:
+            return abs(a[i])
+    return -1
