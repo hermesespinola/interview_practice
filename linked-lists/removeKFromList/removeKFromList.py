6f7cbca71f6ed9701d5a6e69831cdec44889bf65
Hermes <hermes.espinola@wizeline.com>
2018-05-23 18:43:37 -0500

    remove k from list

---
+++ linked-lists/removeKFromList/removeKFromList.py
@@ -0,0 +1,18 @@
+class ListNode(object):
+  def __init__(self, x, n = None):
+    self.value = x
+    self.next = n
+
+def removeKFromList(l, k):
+    current = l
+    while current:
+        if current.next and current.next.value == k:
+            current.next = current.next.next
+        else:
+            current = current.next
+    return l.next if l and l.next.value == k else l
+
+removed = removeKFromList(ListNode(3, ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))), 3)
+while removed != None:
+    print removed.value,
+    removed = removed.next
